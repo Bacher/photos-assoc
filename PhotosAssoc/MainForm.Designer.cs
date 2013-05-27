da7d1bda6f435da91e377f7ec5b9332730f7fcf5
Bacher <bacherko@gmail.com>
2013-05-27 10:31:27 +0400

    Changes

---
+++ PhotosAssoc/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PhotosAssoc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtArciveName = new System.Windows.Forms.TextBox();
+            this.cmdPhotosPathBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPhotosPath = new System.Windows.Forms.TextBox();
+            this.cmdMakeArchive = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmdUpdateXML = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmdXmlBrowse = new System.Windows.Forms.Button();
+            this.txtXmlPath = new System.Windows.Forms.TextBox();
+            this.photosDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.xmlDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtArciveName);
+            this.groupBox1.Controls.Add(this.cmdPhotosPathBrowse);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPhotosPath);
+            this.groupBox1.Controls.Add(this.cmdMakeArchive);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(597, 77);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Архивация фотографий";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Имя архива:";
+            // 
+            // txtArciveName
+            // 
+            this.txtArciveName.Location = new System.Drawing.Point(129, 43);
+            this.txtArciveName.Name = "txtArciveName";
+            this.txtArciveName.Size = new System.Drawing.Size(375, 20);
+            this.txtArciveName.TabIndex = 4;
+            this.txtArciveName.Text = "photos.zip";
+            // 
+            // cmdPhotosPathBrowse
+            // 
+            this.cmdPhotosPathBrowse.Location = new System.Drawing.Point(510, 15);
+            this.cmdPhotosPathBrowse.Name = "cmdPhotosPathBrowse";
+            this.cmdPhotosPathBrowse.Size = new System.Drawing.Size(75, 23);
+            this.cmdPhotosPathBrowse.TabIndex = 3;
+            this.cmdPhotosPathBrowse.Text = "Обзор...";
+            this.cmdPhotosPathBrowse.UseVisualStyleBackColor = true;
+            this.cmdPhotosPathBrowse.Click += new System.EventHandler(this.cmdPhotosPathBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Путь к фотографиям:";
+            // 
+            // txtPhotosPath
+            // 
+            this.txtPhotosPath.Location = new System.Drawing.Point(129, 17);
+            this.txtPhotosPath.Name = "txtPhotosPath";
+            this.txtPhotosPath.Size = new System.Drawing.Size(375, 20);
+            this.txtPhotosPath.TabIndex = 1;
+            // 
+            // cmdMakeArchive
+            // 
+            this.cmdMakeArchive.Location = new System.Drawing.Point(510, 41);
+            this.cmdMakeArchive.Name = "cmdMakeArchive";
+            this.cmdMakeArchive.Size = new System.Drawing.Size(75, 23);
+            this.cmdMakeArchive.TabIndex = 0;
+            this.cmdMakeArchive.Text = "Создать";
+            this.cmdMakeArchive.UseVisualStyleBackColor = true;
+            this.cmdMakeArchive.Click += new System.EventHandler(this.cmdMakeArchive_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmdUpdateXML);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cmdXmlBrowse);
+            this.groupBox2.Controls.Add(this.txtXmlPath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(597, 83);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Обновление XML";
+            // 
+            // cmdUpdateXML
+            // 
+            this.cmdUpdateXML.Location = new System.Drawing.Point(510, 46);
+            this.cmdUpdateXML.Name = "cmdUpdateXML";
+            this.cmdUpdateXML.Size = new System.Drawing.Size(75, 23);
+            this.cmdUpdateXML.TabIndex = 3;
+            this.cmdUpdateXML.Text = "Обновить XML";
+            this.cmdUpdateXML.UseVisualStyleBackColor = true;
+            this.cmdUpdateXML.Click += new System.EventHandler(this.cmdUpdateXML_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Путь к XML:";
+            // 
+            // cmdXmlBrowse
+            // 
+            this.cmdXmlBrowse.Location = new System.Drawing.Point(510, 17);
+            this.cmdXmlBrowse.Name = "cmdXmlBrowse";
+            this.cmdXmlBrowse.Size = new System.Drawing.Size(75, 23);
+            this.cmdXmlBrowse.TabIndex = 1;
+            this.cmdXmlBrowse.Text = "Обзор...";
+            this.cmdXmlBrowse.UseVisualStyleBackColor = true;
+            this.cmdXmlBrowse.Click += new System.EventHandler(this.cmdXmlBrowse_Click);
+            // 
+            // txtXmlPath
+            // 
+            this.txtXmlPath.Location = new System.Drawing.Point(129, 19);
+            this.txtXmlPath.Name = "txtXmlPath";
+            this.txtXmlPath.Size = new System.Drawing.Size(375, 20);
+            this.txtXmlPath.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 186);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Фотографии";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtArciveName;
+        private System.Windows.Forms.Button cmdPhotosPathBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPhotosPath;
+        private System.Windows.Forms.Button cmdMakeArchive;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FolderBrowserDialog photosDialog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button cmdXmlBrowse;
+        private System.Windows.Forms.TextBox txtXmlPath;
+        private System.Windows.Forms.FolderBrowserDialog xmlDialog;
+        private System.Windows.Forms.Button cmdUpdateXML;
+    }
+}
+
---
+++ PhotosAssoc/MainForm.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Xml;
+
+namespace PhotosAssoc
+{
+    public partial class MainForm : Form
+    {
+        Regex dirNamePattern = new Regex(@"\\(\d{3,})[^\\]*$");
+        Regex jpegFileExtensionPattern = new Regex(".jpe?g$", RegexOptions.IgnoreCase);
+        Regex photoNamePattern = new Regex(@"([\d_]*).jpg$");
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdMakeArchive_Click(object sender, EventArgs e)
+        {
+            string tmpDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tmpDir);
+
+            var dirs = Directory.EnumerateDirectories(txtPhotosPath.Text);
+
+            foreach (var dir in dirs) {
+                var match = dirNamePattern.Match(dir);
+                if(match.Groups.Count == 2) {
+                    var id = match.Groups[1].Value;
+                    var files = Directory.EnumerateFiles(dir);
+
+                    var i = 0;
+                    foreach (var file in files) {
+                        if(jpegFileExtensionPattern.IsMatch(Path.GetFileName(file)))
+                        {
+                            File.Copy(file, Path.Combine(tmpDir, string.Format("{0}_{1}.jpg", id, ++i)));
+                        }
+                    }
+                }
+            }
+
+            File.Delete(Path.Combine(txtPhotosPath.Text, txtArciveName.Text));
+
+
+            try {
+                Process zipProcess = new Process();
+                zipProcess.StartInfo.FileName = "7za.exe";
+                zipProcess.StartInfo.Arguments = string.Format(@"a -tzip ""{0}"" ""{1}\*""", Path.Combine(txtPhotosPath.Text, txtArciveName.Text), tmpDir);
+                zipProcess.StartInfo.CreateNoWindow = true;
+                zipProcess.Start();
+                zipProcess.WaitForExit();
+            } catch(Exception ex) {
+                MessageBox.Show("7za archivator not found.");
+            }            
+
+            Directory.Delete(tmpDir, true);
+        }
+
+        private void cmdPhotosPathBrowse_Click(object sender, EventArgs e)
+        {
+            if(photosDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtPhotosPath.Text = photosDialog.SelectedPath;
+        }
+
+        private void cmdXmlBrowse_Click(object sender, EventArgs e)
+        {
+            if (xmlDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtXmlPath.Text = xmlDialog.SelectedPath;
+        }
+
+        private void cmdUpdateXML_Click(object sender, EventArgs e)
+        {
+            string report = "";
+            try {
+                Process zipProcess = new Process();
+                zipProcess.StartInfo.FileName = "7za.exe";
+                zipProcess.StartInfo.Arguments = string.Format(@"l ""{0}""", Path.Combine(txtPhotosPath.Text, txtArciveName.Text));
+                zipProcess.StartInfo.CreateNoWindow = true;
+                zipProcess.StartInfo.RedirectStandardOutput = true;
+                zipProcess.StartInfo.UseShellExecute = false;
+                
+                zipProcess.Start();
+
+                report = zipProcess.StandardOutput.ReadToEnd();
+
+                zipProcess.WaitForExit();
+            } catch (Exception ex) {
+                MessageBox.Show("7za archivator not found.");
+                return;
+            }
+
+            var reportLines = report.Split('\n');
+
+            //List<string> photos = new List<string>();
+
+            Dictionary<int, List<string>> assocPhotos = new Dictionary<int, List<string>>();
+
+            for (int i = 12; i < reportLines.Count() - 3; ++i) {
+                string line = reportLines[i].Trim();
+                var match = photoNamePattern.Match(line);
+                if (match.Groups.Count == 2) {
+
+                    string fileName = match.Groups[1].Value.Trim();
+
+                    int id = int.Parse(fileName.Split('_')[0]);
+
+                    if (!assocPhotos.ContainsKey(id))
+                        assocPhotos[id] = new List<string>();
+                        
+                    assocPhotos[id].Add(fileName);
+                }
+            }
+
+            updateXml(assocPhotos);
+        }
+
+        private void updateXml(Dictionary<int, List<string>> assocPhotos)
+        {
+            var files = Directory.GetFiles(txtXmlPath.Text);
+
+            string outPath = Path.Combine(txtXmlPath.Text, "WithPhotos\\");
+            Directory.CreateDirectory(outPath);
+
+            foreach (var file in files) {
+                if (file.ToLower().EndsWith(".xml")) {
+                    var doc = new XmlDocument();
+                    doc.Load(file);
+
+                    var rowData = doc.DocumentElement.GetElementsByTagName("ROWDATA");
+
+                    if (rowData.Count == 1) {
+                        int index = 0;
+                        var rowDataElement = rowData[0];
+
+                        var rowCount = rowDataElement.ChildNodes.Count;
+
+                        for (int i = 0; i < rowCount; ++i) {
+                            
+                            var line = rowDataElement.ChildNodes[i];
+
+                            var id = line.Attributes["RLT_MAIN_ID"].Value;
+                            id = id.Substring(0, id.Length - 5);
+                            int iid = int.Parse(id);
+                            
+                            var attr = doc.CreateAttribute("RLT_PHOTOS");
+                            attr.Value = "";
+
+                            if (assocPhotos.ContainsKey(iid)) {
+                                assocPhotos[iid].ForEach(str => attr.Value += str + ";");
+                            }
+
+                            line.Attributes.Append(attr);
+                        }
+                    }
+
+                    doc.Save(Path.Combine(outPath, Path.GetFileName(file)));
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            txtPhotosPath.Text = Properties.Settings.Default.photospath;
+            txtXmlPath.Text = Properties.Settings.Default.xmlpath;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.photospath = txtPhotosPath.Text;
+            Properties.Settings.Default.xmlpath = txtXmlPath.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void txtPhotosPath_TextChanged(object sender, EventArgs e)
+        {
+            photosDialog.SelectedPath = this.Text;
+        }
+
+        private void txtXmlPath_TextChanged(object sender, EventArgs e)
+        {
+            xmlDialog.SelectedPath = this.Text;
+        }
+    }
+}
